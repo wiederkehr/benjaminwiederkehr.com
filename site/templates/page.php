93f6cd936c50234bc26117f25b289d07b29d1b6d
Benjamin Wiederkehr <benjamin@interactivethings.com>
2018-01-08 21:28:15 +0100

    Refactoring of the dossier page

--- site/templates/page.php
+++ site/templates/page.php
@@ -1,12 +1,12 @@
 <?= snippet('header', array('style' => 'dossier')) ?>
 <?= snippet('navigation', array('contrast' => true)) ?>
 
-<div class="image-container header-image--<?= strtolower($page->title()) ?>">
+<div class="image-container" style="background-image: url(<?= $page->image('header.png')->url() ?>); background-position: 0 0;">
   <div class="container">
-    <div id="content-header" class="span-18 prepend-6 last">
-      <h1><?= html($page->title()) ?></h1>
-      <h2><?= html($site->title()) ?></h2>
-      <p class="lead"><?= html($page->lead()) ?></p>
+    <div class="content-header span-18 prepend-6 last">
+      <h1 class="content-header__title"><?= html($page->title()) ?></h1>
+      <h2 class="content-header__subtitle"><?= html($site->title()) ?></h2>
+      <p class="content-header__lead"><?= html($page->lead()) ?></p>
     </div>
   </div>
 </div>

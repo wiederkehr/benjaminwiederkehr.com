88ebfc51fa897e94981e8af76f8b244c3291a548
Benjamin Wiederkehr <benjamin@interactivethings.com>
2018-01-15 08:41:23 +0100

    Refine navigation

--- site/snippets/navigation.php
+++ site/snippets/navigation.php
@@ -17,19 +17,19 @@
         <ul class="navigation__list">
           <li class="navigation__item">
             <a class="navigation__link" href="<?= html($site->url()) ?>/about">
-              <span class="navigation__title">About</span>
+              <span class="navigation__title"><i class="fas fa-fw fa-chevron-right"></i> About</span>
               <span class="navigation__claim">Who I am, what I do and how to get in touch with me.</span>
             </a>
           </li>
           <li class="navigation__item">
             <a class="navigation__link" href="<?= html($site->url()) ?>/resume">
-              <span class="navigation__title">Résumé</span>
+              <span class="navigation__title"><i class="fas fa-fw fa-chevron-right"></i> Résumé</span>
               <span class="navigation__claim">How I spend my time and energy.</span>
             </a>
           </li>
           <li class="navigation__item">
             <a class="navigation__link" href="<?= html($site->url()) ?>/events">
-              <span class="navigation__title">Events</span>
+              <span class="navigation__title"><i class="fas fa-fw fa-chevron-right"></i> Events</span>
               <span class="navigation__claim">Upcoming and past appearances.</span>
             </a>
           </li>
@@ -41,19 +41,19 @@
          <ul class="navigation__list">
           <li class="navigation__item">
             <a class="navigation__link" href="<?= html($site->url()) ?>/notes">
-              <span class="navigation__title">Notes</span>
+              <span class="navigation__title"><i class="fas fa-fw fa-chevron-right"></i> Notes</span>
               <span class="navigation__claim">Reflections on the things that keep me busy during the day and up at night.</span>
             </a>
           </li>
           <li class="navigation__item">
             <a class="navigation__link" href="<?= html($site->url()) ?>/talks">
-              <span class="navigation__title">Talks</span>
+              <span class="navigation__title"><i class="fas fa-fw fa-chevron-right"></i> Talks</span>
               <span class="navigation__claim">Thoughts that I have shared in narrated form as talks, presentation and lectures.</span>
             </a>
           </li>
           <li class="navigation__item">
             <a class="navigation__link" href="<?= html($site->url()) ?>/letters">
-              <span class="navigation__title">Letters</span>
+              <span class="navigation__title"><i class="fas fa-fw fa-chevron-right"></i> Letters</span>
               <span class="navigation__claim">Personal messages to the people that live, work, think, play, and travel with me.</span>
             </a>
           </li>

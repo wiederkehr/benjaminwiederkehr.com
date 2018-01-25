1163955cf8c5f6ddbecd1f3a895833777ce10936
Benjamin Wiederkehr <benjamin@interactivethings.com>
2018-01-25 11:13:15 +0100

    Refactor the letters

--- site/templates/letters.php
+++ site/templates/letters.php
@@ -12,9 +12,15 @@
     <!-- ——————————————————————————————————————————————————————————————————— Letter -->
       <div class="letter">
         <a class="letter__link" href="<?= $letter->url() ?>">
-          <span class="letter__date"><?= $letter->date('F j, Y'); ?></span>
-          <h2 class="letter__title" ><?= html($letter->title()) ?></h2>
-          <span class="letter__readmore">Read…</span>
+          <span class="letter__part letter__part--top">
+            <span class="letter__date"><?= $letter->date('F j, Y'); ?></span>
+          </span>
+          <span class="letter__part letter__part--middle">
+            <h2 class="letter__title" ><?= html($letter->title()) ?></h2>
+          </span>
+          <span class="letter__part letter__part--bottom">
+            <span class="letter__readmore">Read…</span>
+          </span>
         </a>
       </div>
       <?php endforeach ?>
